62dddd36ad7fc27363328ef73a8756aee26abb5a
yx <yuanxun@ep-ep.com>
2024-05-08 08:48:31 +0800

    修改了一些bug

--- ep_qrcode/include/ep_qrcode_utility.hpp
+++ ep_qrcode/include/ep_qrcode_utility.hpp
@@ -4,10 +4,10 @@
 
 #include "ros/ros.h"
 #include <tf/tf.h>
-#include <tf2_msgs/TFMessage.h>
 #include <ros/console.h>
 #include <boost/asio.hpp>
 #include <iostream>
+#include <cmath>
 #include <fstream>
 #include <sstream>
 #include <iomanip>
@@ -18,22 +18,28 @@
 #include <array>
 #include "std_msgs/String.h"
 #include <nav_msgs/Odometry.h>
+#include <tf2_msgs/TFMessage.h>
 #include "tf2_ros/transform_listener.h"
+#include "tf2_ros/message_filter.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <geometry_msgs/PoseStamped.h>
+#include "message_filters/subscriber.h"
+#include "tf2_ros/buffer.h"
+#include "geometry_msgs/PointStamped.h"
 
-
-
-//数据帧转换到16进制
-std::string charArrayToHex(std::array<char, 1024> array, size_t size) {
+// 数据帧转换到16进制
+std::string charArrayToHex(std::array<char, 1024> array, size_t size)
+{
     std::stringstream ss;
     ss << std::hex << std::setfill('0');
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i)
+    {
         ss << std::setw(2) << static_cast<int>(static_cast<unsigned char>(array[i]));
     }
     return ss.str();
 }
 
-//十六进制转换为十进制
+// 十六进制转换为十进制
 uint32_t convert_16_to_10(std::string str2)
 {
 
@@ -42,25 +48,25 @@ uint32_t convert_16_to_10(std::string str2)
     std::string::size_type sz = str2.size();
     for (std::string::size_type index = 0; index != sz; ++index)
     {
-        //变为小写，这个思路很好
+        // 变为小写，这个思路很好
         str2[index] = tolower(str2[index]);
         if (str2[index] >= 'a' && str2[index] <= 'f')
         {
-            //这里让a~f进行转换为数字字符，很奇妙
+            // 这里让a~f进行转换为数字字符，很奇妙
             m = str2[index] - 'a' + 10;
-            //求幂次方
+            // 求幂次方
             times = pow(16, (sz - 1 - index));
             sum += m * times;
- 
-        }else if (isdigit(str2[index]))
+        }
+        else if (isdigit(str2[index]))
         {
-            //需要将字符类型转换为数字类型
-            //因为0的ASCII码是48，所以转换为相应的数字，减去48即可
-            m= str2[index] - 48;
+            // 需要将字符类型转换为数字类型
+            // 因为0的ASCII码是48，所以转换为相应的数字，减去48即可
+            m = str2[index] - 48;
             times = pow(16, (sz - 1 - index));
             sum += m * times;
-            
-        }else
+        }
+        else
         {
             std::cout << "无法识别的十六进制!";
             break;
@@ -69,8 +75,9 @@ uint32_t convert_16_to_10(std::string str2)
     return uint32_t(sum);
 }
 
-//时间格式化输出
-std::string format_time(ros::Time t){
+// 时间格式化输出
+std::string format_time(ros::Time t)
+{
     std::stringstream ss;
     ros::WallTime wall_time = ros::WallTime(t.toSec());
     ss << wall_time.toBoost().date().year() << '-';
@@ -79,12 +86,13 @@ std::string format_time(ros::Time t){
     ss << wall_time.toBoost().time_of_day().hours() + 8 << ':';
     ss << wall_time.toBoost().time_of_day().minutes() << ':';
     int second = wall_time.toBoost().time_of_day().seconds();
-    ss << (double(second)*1e3 + double(t.nsec)/1000000.0)/1000.0; // 毫秒
+    ss << (double(second) * 1e3 + double(t.nsec) / 1000000.0) / 1000.0; // 毫秒
     return ss.str();
 }
 
-//扫码相机数据帧格式
-struct frame{
+// 扫码相机数据帧格式
+struct frame
+{
     std::string hex;
     std::string head;
     uint32_t index;
@@ -98,25 +106,31 @@ struct frame{
     std::string sender;
 };
 
-//二维码信息
-struct qrcode_info{
+// 二维码信息
+struct qrcode_info
+{
     uint32_t code;
     double x;
     double y;
     double yaw;
 };
 
-//二维码坐标对照表
-class coordinate_table{
+// 二维码坐标对照表
+class coordinate_table
+{
 public:
-    coordinate_table(std::string path, std::ostream& log_os_){
-        log_os = &log_os_;
+    coordinate_table(std::string path_, std::ofstream *log_os_, geometry_msgs::TransformStamped transform_)
+    {
+        log_os = log_os_;
+        tf_b2c = transform_;
+        path = path_;
         ifs.open(path, std::ios::in);
-        if (!ifs.is_open()){
+        if (!ifs.is_open())
+        {
             *log_os << path + "打开失败!" << std::endl;
         }
-        std::string buf;//将数据存放到c++ 中的字符串中
-        while (std::getline(ifs,buf)) //使用全局的getline()函数，其里面第一个参数代表输入流对象，第一个参数代表准备好的字符串，每次读取一行内容到buf
+        std::string buf;               // 将数据存放到c++ 中的字符串中
+        while (std::getline(ifs, buf)) // 使用全局的getline()函数，其里面第一个参数代表输入流对象，第一个参数代表准备好的字符串，每次读取一行内容到buf
         {
             std::stringstream line_ss;
             line_ss << buf;
@@ -126,54 +140,142 @@ public:
         }
         ifs.close();
         *log_os << "qrcode_table的大小为: " << map.size() << std::endl;
-        for(std::map<uint32_t, qrcode_info>::iterator it = map.begin(); it != map.end(); it++) {
-            *log_os << (*it).second.code << " " <<  (*it).second.x << " " <<  (*it).second.y << " " <<  (*it).second.yaw << std::endl;
+        for (std::map<uint32_t, qrcode_info>::iterator it = map.begin(); it != map.end(); it++)
+        {
+            *log_os << (*it).second.code << " " << (*it).second.x << " " << (*it).second.y << " " << (*it).second.yaw << std::endl;
         }
     }
 
-    ~coordinate_table(){}
+    ~coordinate_table() {}
 
-    bool find(frame pic, qrcode_info* info){
-        std::map<uint32_t, qrcode_info>::iterator it = map.find(code);
-        if (it != map.end()) {
+    // 根据二维码编号查表，得到位姿信息
+    bool find(frame pic, qrcode_info *info)
+    {
+        std::map<uint32_t, qrcode_info>::iterator it = map.find(pic.code);
+        if (it != map.end())
+        {
             *info = (*it).second;
             return true;
-        } else {
+        }
+        else
+        {
             *log_os << "can not identify code:" << pic.code << std::endl;
-            addframe(pic);
+            add(pic);
         }
         return false;
     }
 
-    bool addframe(frame pic){
-        std::lock_guard<std::mutex> locker(mtx);
-        //
-        return false;
+    // 添加新的二维码
+    bool add(const frame pic)
+    {
+        // std::lock_guard<std::mutex> locker(coordinate_table::mtx);
+        if (frame_buffer.size() > 120)
+        {
+            frame_buffer.pop_front();
+        }
+        static uint32_t lastcode = 0;
+        if (pic.code != lastcode)
+        {
+            lastcode = pic.code;
+            frame_buffer.clear();
+        }
+        frame_buffer.push_back(pic);
+
+        // 计算并保存新二维码信息
+        if ((tf_buffer.size() > 9) || (frame_buffer.size() > 110))
+        {
+            qrcode_info new_qrcode = calPose();
+            new_qrcode.code = pic.code;
+            map.insert(std::pair<uint32_t, qrcode_info>(new_qrcode.code, new_qrcode));
+            std::ofstream save(path, std::ios::app);
+            std::stringstream line_ss;
+            std::ostream &save_os = save; // 将文件流转换为输出流对象
+            save_os << new_qrcode.code << " " << new_qrcode.x << " " << new_qrcode.y << " " << new_qrcode.yaw << std::endl;
+            save.close();
+        }
+
+        return true;
     }
 
-    /* 
-    //采集二维码信息
-    std::ofstream table_ofs;
-    if(collect_QRcode){
-        std::string log_name = log_dir + "/"+ format_time(ros::Time::now()) +".txt";
-        std::ofstream log_file(log_name);
-        if (!log_file.is_open()) {
-            std::cout << "can't open: " << log_name << std::endl;
-        }else{
-            std::cout << "open: " << log_name << std::endl;
-        }
-        
-    }
-    std::ostream& log_os = log_file; // 将文件流转换为输出流对象 */
+    // 计算二维码位姿
+    qrcode_info calPose()
+    {
+        std::lock_guard<std::mutex> locker(coordinate_table::mtx);
+        double x_sum = 0, y_sum = 0, yaw_sum = 0;
+        int i = 0;
+        std::list<qrcode_info> poselog;
+        for (std::list<nav_msgs::Odometry>::iterator t_it = tf_buffer.begin(); t_it != tf_buffer.end(); t_it++)
+        {
+            double min_time_err = 1.0;
+            std::list<frame>::iterator d_it = frame_buffer.begin();
+            for (std::list<frame>::iterator f_it = frame_buffer.begin(); f_it != frame_buffer.end(); f_it++)
+            {
+                ros::Duration time_err = f_it->stamp - t_it->header.stamp;
+                double d_time_err = std::fabs(time_err.toSec());
+                if (d_time_err > min_time_err)
+                {
+                    continue;
+                }
+                else
+                {
+                    min_time_err = d_time_err;
+                    d_it = f_it;
+                }
+            }
+            // baselink---->camera
+            geometry_msgs::Pose pose_out;
+            tf2::doTransform(t_it->pose.pose, pose_out, tf_b2c);
+            double roll = 0.0, pitch = 0.0, yaw = 0.0;                                                                        // 初始化欧拉角
+            tf::Quaternion q(pose_out.orientation.x, pose_out.orientation.y, pose_out.orientation.z, pose_out.orientation.w); // 初始化四元数
+            tf::Matrix3x3(q).getRPY(roll, pitch, yaw);                                                                        // 四元数转欧拉角
 
-    static void tfCallback(const tf2_msgs::TFMessage::ConstPtr& msg) {
-        std::lock_guard<std::mutex> locker(mtx);
-        for (int i = 0; i < msg->transforms.size(); ++i) {
-            // 处理每个变换, 例如打印出来
-            ROS_INFO("Received transform: %s", msg->transforms[i].child_frame_id.c_str());
-            tf_buffer.push_back(msg->transforms[i]);
+            // camera---->qrcode
+            qrcode_info frame;
+            frame.x = pose_out.position.x - d_it->error_x;
+            frame.y = pose_out.position.y - d_it->error_y;
+            frame.yaw = yaw - d_it->error_yaw;
+            poselog.push_back(frame);
+
+            x_sum += frame.x;
+            y_sum += frame.y;
+            yaw_sum += frame.yaw;
+            i++;
         }
-        while(20 < tf_buffer.size()){
+
+        // //tode:检查10帧计算结果之间的偏差
+        // for(std::list<qrcode_info> it = poselog.begin() + 1; it != poselog.end(); it++){
+        // }
+
+        // 输出平均后的结果
+        qrcode_info new_frame;
+        new_frame.x = x_sum / i;
+        new_frame.y = y_sum / i;
+        new_frame.yaw = yaw_sum / i;
+
+        return (new_frame);
+    }
+
+    // callback获取baselink位姿
+    static void tfCallback(const nav_msgs::Odometry::ConstPtr &msg)
+    {
+        std::lock_guard<std::mutex> locker(coordinate_table::mtx);
+        const nav_msgs::Odometry transform = *msg; //->transforms[i];
+        // *log_os << "base_link at:" << transform.pose.pose.position.x << " "
+        //         << transform.pose.pose.position.y << " "
+        //         << transform.pose.pose.position.z << " "
+        //         << std::endl;
+        if (tf_buffer.size() != 0)
+        {
+            double x_error = std::fabs(transform.pose.pose.position.x - tf_buffer.back().pose.pose.position.x);
+            double y_error = std::fabs(transform.pose.pose.position.y - tf_buffer.back().pose.pose.position.y);
+            if ((x_error > 0.05) || (y_error > 0.05))
+            {
+                tf_buffer.clear();
+            }
+        }
+        tf_buffer.push_back(transform);
+        while (10 < tf_buffer.size())
+        {
             tf_buffer.pop_front();
         }
     }
@@ -182,11 +284,17 @@ private:
     std::ifstream ifs;
     std::map<uint32_t, qrcode_info> map;
     std::map<uint32_t, qrcode_info> new_map;
-    std::ostream* log_os;
-    static std::list<tf2_msgs::TFMessage::transforms> tf_buffer;
-    std::mutex mtx;
+    std::ofstream *log_os;
+    std::list<frame> frame_buffer;
 
+    geometry_msgs::TransformStamped tf_b2c;
+    std::string path;
+
+    static std::mutex mtx;
+    static std::list<nav_msgs::Odometry> tf_buffer;
 };
 
+std::mutex coordinate_table::mtx;
+std::list<nav_msgs::Odometry> coordinate_table::tf_buffer;
 
 #endif
--- ep_qrcode/src/main.cpp
+++ ep_qrcode/src/main.cpp
@@ -1,22 +1,23 @@
-//################################################
-//#####        main function for ep_qrcode                     
-//#####        yuanxun@ep-ep.com                     
-//#####        updateTime:  2024.05.01  
-//################################################
+// ################################################
+// #####        main function for ep_qrcode
+// #####        yuanxun@ep-ep.com
+// #####        updateTime:  2024.05.01
+// ################################################
 #include "ep_qrcode_utility.hpp"
 #include "MV_SC2005AM.hpp"
 
-//主函数
+// 主函数
 int main(int argc, char *argv[])
 {
-    //初始化
-    setlocale(LC_ALL,"");
-    ros::init(argc,argv,"ep_qrcode");
+    // 初始化
+    setlocale(LC_ALL, "");
+    ros::init(argc, argv, "ep_qrcode");
     ros::NodeHandle nh;
 
-    //读取参数
-    std::string odomTopic;;
-    std::string msgTopic;;
+    // 读取参数
+    std::string odomTopic;
+    std::string msgTopic;
+
     bool show_msg;
     bool collect_QRcode;
     std::string port;
@@ -30,76 +31,80 @@ int main(int argc, char *argv[])
     nh.param<std::string>("ep_qrcode/log_dir", log_dir, "/var/xmover/log/qrcode");
     nh.param<std::string>("ep_qrcode/cfg_dir", cfg_dir, "/var/xmover/params");
 
-    //发布器
-    ros::Publisher pub_qrCodeMsg = nh.advertise<std_msgs::String>(msgTopic,1000);
-    ros::Publisher pub_qrcode_odom = nh.advertise<nav_msgs::Odometry> (odomTopic, 2000);
+    // 发布器
+    ros::Publisher pub_qrCodeMsg = nh.advertise<std_msgs::String>(msgTopic, 1000);
+    ros::Publisher pub_qrcode_odom = nh.advertise<nav_msgs::Odometry>(odomTopic, 2000);
 
-    //日志文件初始化
-    std::string log_name = log_dir + "/"+ format_time(ros::Time::now()) +".txt";
+    // 日志文件初始化
+    std::string log_name = log_dir + "/" + format_time(ros::Time::now()) + ".txt";
     std::ofstream log_file(log_name);
-    if (!log_file.is_open()) {
+    if (!log_file.is_open())
+    {
         std::cout << "can't open: " << log_name << std::endl;
-    }else{
+    }
+    else
+    {
         std::cout << "open: " << log_name << std::endl;
     }
-    std::ostream& log_os = log_file; // 将文件流转换为输出流对象
 
-    coordinate_table table(cfg_dir + "/qrcode_coordinate_table.txt", log_os);
-    MV_SC2005AM camera(port, log_os);
-
-    ros::Subscriber* sub_pos;
-    ros::Subscriber* sub_trans;
-    if(collect_QRcode){
-        *sub_pos = nh.subscribe<tf2_msgs::TFMessage>("/tf", 1000, table.tfCallback);
-    }
-
-    tf::Transform*  tfListener= new tf::TransformListener;
-    tf::StampedTransform stfBaseToLink2, stfBaseToLink1, stfLink1ToLink2;
+    // baselink---->camera的变换关系
+    tf2_ros::Buffer buffer;
+    tf2_ros::TransformListener tfListener(buffer);
+    geometry_msgs::TransformStamped transform;
     bool tferr = true;
-    while (tferr) {
-        tferr=false;
-        try{
-            tfListener.lookupTransform("/map", "/base_link",ros::Time(0), transform);
-            tfListener->lookupTransform("base_link", "link1", ros::Time(0), tfLink2WrtBaseLink);
-        }catch(tf::TransformException &exception) {
+    while (tferr)
+    {
+        tferr = false;
+        try
+        {
+            transform = buffer.lookupTransform("sc2005am_link", "base_link", ros::Time(0));
+        }
+        catch (tf::TransformException &exception)
+        {
             ROS_WARN("%s; retrying...", exception.what());
-            tferr=true;
-            ros::Duration(0.5).sleep(); 
-            continue;            
-        }   
+            tferr = true;
+            ros::Duration(0.5).sleep();
+            continue;
+        }
     }
 
+    coordinate_table table(cfg_dir + "/qrcode_coordinate_table.txt", &log_file, transform);
+    MV_SC2005AM camera(port, &log_file);
 
-    
-    //主循环 100Hz
+    ros::Subscriber sub_pos = nh.subscribe<nav_msgs::Odometry>("/ep_localization/odometry/lidar", 1000, &coordinate_table::tfCallback);
+
+    // 主循环 100Hz
     ros::Rate loop_rate(100);
     while (ros::ok())
-    {   
-        //查询相机数据
-        frame pic; 
+    {
+        // 查询相机数据
+        frame pic;
         static nav_msgs::Odometry odom;
-        if(camera.getframe(&pic)){
-            //发布 /qrCodeMsg
-            if(show_msg){
+        if (camera.getframe(&pic))
+        {
+            // 发布 /qrCodeMsg
+            if (show_msg)
+            {
                 std::stringstream pub_ss;
-                pub_ss  << format_time(pic.stamp) << " [" << pic->sender << "] " << pic.index << " " << pic.duration << "ms " // ip
-                        << " " << pic.error_x << "mm "<< pic.error_y << "mm "<< pic.error_yaw << std::endl;
+                pub_ss << format_time(pic.stamp) << " [" << pic.sender << "] " << pic.index << " " << pic.duration << "s " // ip
+                       << " " << pic.error_x << "mm " << pic.error_y << "mm " << pic.error_yaw << std::endl;
                 std_msgs::String msg;
                 msg.data = pub_ss.str();
                 pub_qrCodeMsg.publish(msg);
             }
-            
-            //查询二维码坐标
+
+            // 查询二维码坐标
             qrcode_info code_info;
-            if(table.find(pic, &code_info)){
-                //计算相机坐标
+            if (table.find(pic, &code_info))
+            {
+                // 计算相机坐标
                 double camera_x = code_info.x + pic.error_x;
                 double camera_y = code_info.y + pic.error_y;
                 double camera_yaw = code_info.yaw + pic.error_yaw;
                 tf::Quaternion q;
                 q.setRPY(0, 0, camera_yaw);
 
-                //发布相机坐标
+                // 发布相机坐标
                 odom.header.stamp = pic.stamp;
                 odom.header.frame_id = "map";
                 odom.header.seq = pic.index;
@@ -110,23 +115,25 @@ int main(int argc, char *argv[])
                 odom.pose.pose.orientation.y = q.getY();
                 odom.pose.pose.orientation.z = q.getZ();
                 odom.pose.pose.orientation.w = q.getW();
-                odom.pose.covariance[0] = 1; //此帧是否可用，1：可用，0：不可用
-                odom.pose.covariance[1] = pic.duration; //相机处理图像用时(s)
-                pub_qrcode_odom.publish(odom);
-            }else{
-                odom.pose.covariance[0] = 0; //此帧是否可用，1：可用，0：不可用
+                odom.pose.covariance[0] = 1;            // 此帧是否可用，1：可用，0：不可用
+                odom.pose.covariance[1] = pic.duration; // 相机处理图像用时(s)
                 pub_qrcode_odom.publish(odom);
             }
-        }else{
-            odom.pose.covariance[0] = 0; //此帧是否可用，1：可用，0：不可用
+            else
+            {
+                odom.pose.covariance[0] = 0; // 此帧是否可用，1：可用，0：不可用
+                pub_qrcode_odom.publish(odom);
+            }
+        }
+        else
+        {
+            odom.pose.covariance[0] = 0; // 此帧是否可用，1：可用，0：不可用
             pub_qrcode_odom.publish(odom);
         }
- 
+
         loop_rate.sleep();
         ros::spinOnce();
     }
 
     return 0;
 }
-
-
